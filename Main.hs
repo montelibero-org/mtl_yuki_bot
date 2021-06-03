3451158f6d8dd67e7ea98faae27e9ecf57055276
Yuriy Syrovetskiy <git-orbi@cblp.su>
2021-06-03 22:33:49 +0300

    Add /mtl_holders

--- Main.hs
+++ Main.hs
@@ -1,9 +1,18 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Applicative ((<|>))
+import           Control.Lens ((^.))
+import           Control.Monad.IO.Class (liftIO)
+import           Data.Aeson (FromJSON)
+import qualified Data.Text as Text
+import           GHC.Generics (Generic)
+import           Network.Wreq (asJSON, get, responseBody)
 import           Telegram.Bot.API (Update, defaultTelegramClientEnv)
 import           Telegram.Bot.Simple (BotApp (..), BotM, Eff, eff, getEnvToken,
                                       replyText, startBot_, (<#))
@@ -20,7 +29,7 @@ main = do
 newtype Model = Model{initialized :: Bool}
   deriving Show
 
-data Command = Help
+data Command = Help | MtlInfo | MtlHolders
   deriving Show
 
 data Action = NoAction | Action Command
@@ -37,8 +46,10 @@ botApp =
 
 handleUpdate :: Update -> Maybe Command
 handleUpdate =
-  parseUpdate $
-  Help <$ (UpdateParser.command "help" <|> UpdateParser.command "start")
+  parseUpdate
+    $   Help <$ (UpdateParser.command "help" <|> UpdateParser.command "start")
+    <|> MtlInfo    <$ UpdateParser.command "mtl"
+    <|> MtlHolders <$ UpdateParser.command "mtl_holders"
 
 handleAction :: Action -> Model -> Eff Action Model
 handleAction action model@Model{initialized = False} = do
@@ -57,4 +68,61 @@ initialize = pure ()
 handleCommand :: Command -> BotM ()
 handleCommand = \case
   Help ->
-    replyText "This bot show some stats about MTL fund and its affiliated funds"
+    replyText
+      "This bot show some stats about MTL fund and its affiliated funds.\n\
+      \\n\
+      \/mtl – MTL information\n\
+      \/mtl_holders – MTL holders\n\
+      \"
+  MtlInfo ->
+    replyText
+      "https://stellar.expert/explorer/public/asset/\
+      \MTL-GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V"
+  MtlHolders -> do
+    holders <- liftIO $ getHolders mtl
+    replyText $ Text.pack $ show holders
+
+getHolders :: Fund -> IO [Holder]
+getHolders fund =
+  do
+    r <- asJSON =<< get url
+    let ResponseOk{_embedded = Embedded{records}} = r ^. responseBody
+    pure records
+  where
+    network = "public"
+    url =
+      concat
+        [ "https://api.stellar.expert/explorer/", network
+        , "/asset/", assetId fund
+        , "/holders"
+        ]
+
+mtl :: Fund
+mtl =
+  Fund
+    { assetName   = "MTL"
+    , assetIssuer = "GACKTN5DAZGWXRWB2WLM6OPBDHAMT6SJNGLJZPQMEZBUR4JUGBX2UK7V"
+    , treasury = Just "GDX23CPGMQ4LN55VGEDVFZPAJMAUEHSHAMJ2GMCU2ZSHN5QF4TMZYPIS"
+    }
+
+-- mtlcity :: Fund
+-- mtlcity =
+--   Fund
+--     { assetName   = "MTLCITY"
+--     , assetIssuer = "GDUI7JVKWZV4KJVY4EJYBXMGXC2J3ZC67Z6O5QFP4ZMVQM2U5JXK2OK3"
+--     , treasury    = Nothing
+--     }
+
+newtype ResponseOk a = ResponseOk{_embedded :: Embedded a}
+  deriving (FromJSON, Generic)
+
+newtype Embedded a = Embedded{records :: [a]}
+  deriving (FromJSON, Generic)
+
+data Holder = Holder{account, balance :: String}
+  deriving (FromJSON, Generic, Show)
+
+data Fund = Fund{assetName, assetIssuer :: String, treasury :: Maybe String}
+
+assetId :: Fund -> String
+assetId Fund{assetName, assetIssuer} = assetName <> "-" <> assetIssuer
